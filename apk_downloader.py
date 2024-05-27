755cb1b56db5b3263f0bade88c3ed9c52b5999ed
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-05-27 20:55:59 +0700

    Update apk_downloader.py

--- apk_downloader.py
+++ apk_downloader.py
@@ -9,68 +9,28 @@ scraper = cloudscraper.create_scraper(
     }
 )
 
-def get_download_page(version: str) -> str:
-    apkmirror_url = "https://www.apkmirror.com"
-    apkmirror_yt_url = (
-        f"https://www.apkmirror.com/apk/google-inc/youtube-music/youtube-music"
-        + f"-{version.replace('.', '-')}-release/"
-    )
-
-    response = scraper.get(apkmirror_yt_url)
-    response.raise_for_status()
-
-    soup = BeautifulSoup(response.content, "html.parser")
-    yt_links = soup.find_all("div", attrs={"class": "table-row headerFont"})
-    yt_apk_page = apkmirror_url
-
-    for link in yt_links[1:]:
-        if link.find_all("span", attrs={"class": "apkm-badge"})[0].text == "APK":
-            yt_apk_page += link.find_all("a", attrs={"class": "accent_color"})[0]["href"]
-            break
-
-    return yt_apk_page
-
-def extract_download_link(page: str) -> str:
-    apkmirror_url = "https://www.apkmirror.com"
-
-    res = scraper.get(page)
-    res.raise_for_status()
-
-    soup = BeautifulSoup(res.content, "html.parser")
-    apk_dl_page = soup.find_all("a", attrs={"class": "accent_bg"})
-    apk_dl_page_url = apkmirror_url + apk_dl_page[0]["href"]
-
-    res = scraper.get(apk_dl_page_url)
-    res.raise_for_status()
-
-    soup = BeautifulSoup(res.content, "html.parser")
-    apk_page_details = soup.find_all("a", attrs={"rel": "nofollow"})
-    apk_link = apkmirror_url + apk_page_details[0]["href"]
-
-    return apk_link
-
-def download_file_with_cloudscraper(url: str, filename: str):
-    with open(filename, "wb") as f:
-        scraper = cloudscraper.create_scraper()
-        response = scraper.get(url, stream=True)
-        if response.status_code == 200:
-            for chunk in response.iter_content(1024):
-                f.write(chunk)
-        else:
-            logging.error(f"Failed to download file from {url}")
-
-# Ví dụ sử dụng:
 version = "7.02.51"
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-try:
-    download_page_url = get_download_page(version)
-    download_link = extract_download_link(download_page_url)
-    logging.info(f"Download link for YouTube version {version}: {download_link}")
+apkmirror_url = "https://www.apkmirror.com"
+apkmirror_yt_url = (
+    f"https://www.apkmirror.com/apk/google-inc/youtube-music/youtube-music"
+    + f"-{version.replace('.', '-')}-release/"
+)
 
-    # Tên file để lưu
-    filename = f"YouTube_v{version}.apk"
-    # Thực hiện tải tệp
-    download_file_with_cloudscraper(download_link, filename)
-    logging.info(f"File downloaded successfully as {filename}")
-except Exception as e:
-    logging.erro(f"Error: {e}")
+response = scraper.get(apkmirror_yt_url)
+response.raise_for_status()
+
+soup = BeautifulSoup(response.content, "html.parser")
+links = soup.find_all('a', class_='accent_color')
+
+# Khởi tạo danh sách lưu các URL hợp lệ
+valid_urls = []
+
+for link in links:
+    # Kiểm tra phần tử tiếp theo của thẻ <a>
+    next_sibling = link.find_next_sibling()
+    if next_sibling and next_sibling.get_text() == 'APK':
+        next_next_sibling = next_sibling.find_next_sibling()
+        if next_next_sibling and (next_next_sibling.get_text() == 'arm64-v8a' or next_next_sibling.get_text() == 'nodpi'):
+            valid_urls.append(link['href'])
+
+print(valid_urls)
