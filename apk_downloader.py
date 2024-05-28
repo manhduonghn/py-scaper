83e62fee82951a35a2e90499585f274b733f1f65
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-05-28 08:58:25 +0700

    Update apk_downloader.py

--- apk_downloader.py
+++ apk_downloader.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 keywords = ["APK", "universal", "nodpi"]
 base_url = "https://www.apkmirror.com"
 
+# Create a scraper with a custom user agent
 scraper = cloudscraper.create_scraper(
     browser={
         'custom': 'Mozilla/5.0'
@@ -11,27 +12,33 @@ scraper = cloudscraper.create_scraper(
 )
 
 def get_download_page(version: str) -> str:
-    
+    """
+    Get the download page URL for a specific YouTube version.
+    """
     yt_url = f"{base_url}/apk/google-inc/youtube/youtube-{version.replace('.', '-')}-release/"
     response = scraper.get(yt_url)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, "html.parser")
 
+    # Iterate through all links with class 'accent_color'
     for link in soup.find_all('a', class_='accent_color'):
-        parent_div = link.find_parent('div', class='table-cell')
+        parent_div = link.find_parent('div', class_='table-cell')
         if not parent_div:
             continue
 
         texts = [parent_div.get_text(strip=True)] + \
                 [sibling.get_text(strip=True) for sibling in parent_div.find_next_siblings('div')]
 
+        # Check if all keywords are present in the texts
         if all(any(keyword.lower() in text.lower() for text in texts) for keyword in keywords):
             return f"{base_url}{link['href']}"
 
     return None
 
 def extract_download_link(page_url: str) -> str:
-    
+    """
+    Extract the actual download link from the download page.
+    """
     response = scraper.get(page_url)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, "html.parser")
