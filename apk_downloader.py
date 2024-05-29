b6ca6d7a04dce6af1104c437577580a499ef62d6
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-05-29 13:26:52 +0700

    Update apk_downloader.py

--- apk_downloader.py
+++ apk_downloader.py
@@ -13,55 +13,21 @@ scraper = cloudscraper.create_scraper(
     }
 )
 
-def get_download_page(version: str) -> str:
-    base_url = "https://www.apkmirror.com"
-    url = f"{base_url}/apk/facebook-2/messenger/messenger-{version.replace('.', '-')}-release/"
+url = f"https://www.apkmirror.com/uploads/?appcategory=messenger"
 
-    response = scraper.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.content, "html.parser")
+response = scraper.get(url)
+response.raise_for_status()
+soup = BeautifulSoup(response.content, "html.parser")
 
-    for href_content in soup.find_all('a', class_='accent_color'):
-        parent = href_content.find_parent('div', class_='table-cell')
-        if parent:
-            infos = [parent.get_text(strip=True)] + [sib.get_text(strip=True) for sib in parent.find_next_siblings('div')]
-            if all(any(keyword in info for info in infos) for keyword in keywords):
-                return base_url + href_content['href']
+# Tìm tất cả các phiên bản ứng dụng
+app_rows = soup.find_all("div", class_="appRow")
 
-    return None
+# Duyệt qua từng phiên bản để tìm phiên bản cao nhất không phải alpha hoặc beta
+latest_version = None
+for row in app_rows:
+    version = row.find("h5", class_="appRowTitle").a.text.strip()
+    if "alpha" not in version.lower() and "beta" not in version.lower():
+        latest_version = version
+        break
 
-def extract_download_link(page: str) -> str:
-    base_url = "https://www.apkmirror.com"
-
-    response = scraper.get(page)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    href_content = soup.find('a', class_='downloadButton')
-    if href_content:
-        download_page_url = base_url + href_content['href']
-        response = scraper.get(download_page_url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        href_content = soup.select_one('a[rel="nofollow"]')['href']
-        if href_content:
-            return base_url + href_content
-
-    return None
-
-# Ví dụ sử dụng
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-version = "458.0.0.54.108"
-download_page = get_download_page(version)
-if download_page:
-    download_link = extract_download_link(download_page)
-    print("Valid URL:", download_link)
-    if download_link:
-        filename = f"messenger-v{version}.apk"
-        with open(filename, 'wb') as f:
-            response = scraper.get(download_link)
-            f.write(response.content)
-        print("File downloaded successfully as", filename)
-else:
-    print("No valid download page found.")
+print("Phiên bản mới nhất không phải alpha hoặc beta là:", latest_version)
