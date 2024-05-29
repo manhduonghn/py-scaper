4120201ae7f609db1d30194851ebc8fd807f028b
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-05-29 13:35:46 +0700

    Update apk_downloader.py

--- apk_downloader.py
+++ apk_downloader.py
@@ -2,6 +2,7 @@ import cloudscraper
 import logging
 from bs4 import BeautifulSoup
 import os
+import re
 
 # Tạo một scraper với thông tin trình duyệt tùy chỉnh
 scraper = cloudscraper.create_scraper(
@@ -10,7 +11,7 @@ scraper = cloudscraper.create_scraper(
     }
 )
 
-url = f"https://www.apkmirror.com/uploads/?appcategory=youtube-music"
+url = f"https://www.apkmirror.com/uploads/?appcategory=youtube"
 
 response = scraper.get(url)
 response.raise_for_status()
@@ -19,12 +20,17 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Tìm tất cả các phiên bản ứng dụng
 app_rows = soup.find_all("div", class_="appRow")
 
+# Chuẩn bị biểu thức chính quy để tìm số phiên bản
+version_pattern = re.compile(r'\d+(\.\d+)+')
+
 # Duyệt qua từng phiên bản để tìm phiên bản cao nhất không phải alpha hoặc beta
 latest_version = None
 for row in app_rows:
-    version = row.find("h5", class_="appRowTitle").a.text.strip()
-    if "alpha" not in version.lower() and "beta" not in version.lower():
-        latest_version = version
-        break
+    version_text = row.find("h5", class_="appRowTitle").a.text.strip()
+    if "alpha" not in version_text.lower() and "beta" not in version_text.lower():
+        match = version_pattern.search(version_text)
+        if match:
+            latest_version = match.group()
+            break
 
 print("Phiên bản mới nhất không phải alpha hoặc beta là:", latest_version)
