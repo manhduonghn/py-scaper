2f6e54f40c6d7e4b0719635326afab3e153c9aa0
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-01 00:27:00 +0700

    Update apk_downloader.py

--- apk_downloader.py
+++ apk_downloader.py
@@ -19,20 +19,24 @@ def get_download_page(version: str) -> str:
     response.raise_for_status()
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # Tìm tất cả các thẻ div có thuộc tính data-url
     divs = soup.find_all("div", {"data-url": True})
 
-    # Duyệt qua từng thẻ div để tìm version tương ứng
     for div in divs:
         version_span = div.find("span", class_="version")
         if version_span and version_span.text.strip() == version:
-            data_url = div["data-url"]
-            data_url = data_url.replace('/download/', '/post-download/')
-            logging.debug("Data URL của phiên bản %s là: %s", version, data_url)
-            return data_url
+            dl_page = div["data-url"]
+            dl_url = dl_page.replace('/download/', '/post-download/')
+            return dl_url
 
-    # Nếu không tìm thấy data-url, trả về None
-    logging.debug("Không tìm thấy data-url cho phiên bản %s", version)
+    response = scraper.get(dl_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, "html.parser")
+    
+    post_download_divs = soup.find_all("div", class_="post-download")
+    for div in post_download_divs:
+        data_url = div["data-url"]
+        print("Data URL:", data_url)
+
     return None
 
 version = "19.20.32"
