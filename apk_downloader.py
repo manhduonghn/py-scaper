e1b81e7570add6fbe936f8f45a4b99ab5617df8e
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-01 00:09:04 +0700

    Update apk_downloader.py

--- apk_downloader.py
+++ apk_downloader.py
@@ -1,7 +1,5 @@
-import os
-import re
-import cloudscraper
 import logging
+import cloudscraper
 from bs4 import BeautifulSoup
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -13,33 +11,20 @@ scraper = cloudscraper.create_scraper(
 )
 
 def get_download_page(version: str) -> str:
-    url = f"https://youtube.en.uptodown.com/android/versions"
-
+    url = "https://youtube.en.uptodown.com/android/versions"
     response = scraper.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, "html.parser")
-
-    # Tìm tất cả các thẻ div có thuộc tính data-url
     divs = soup.find_all("div", {"data-url": True})
 
-    # Biến để kiểm soát việc dừng tìm kiếm
-    found = False
-
-    # Duyệt qua từng thẻ div để tìm version tương ứng
     for div in divs:
         version_span = div.find("span", class_="version")
         if version_span and version_span.text.strip() == version:
-            data_url = div["data-url"]
-            logging.debug("Data URL của phiên bản %s là: %s", version, data_url)
-            found = True
-            break
+            data_url = div["data-url"].replace('/download/', '/post-download/')
+            return data_url
 
-    # Nếu tìm thấy data-url, trả về nó
-    if found:
-        return data_url
-    else:
-        logging.debug("Không tìm thấy data-url cho phiên bản %s", version)
-        return ""
+    return None
 
 version="19.20.32"
-get_download_page(version)
+data-url = get_download_page(version)
+print (data-url)
