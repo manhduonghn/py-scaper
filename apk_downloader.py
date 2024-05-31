a3ab14ad4b197f39ae12183f39d0ab66b0340381
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-01 00:41:20 +0700

    Update apk_downloader.py

--- apk_downloader.py
+++ apk_downloader.py
@@ -17,21 +17,18 @@ def get_download_link(version: str) -> str:
     response.raise_for_status()
     soup = BeautifulSoup(response.content, "html.parser")
 
-    divs = soup.find_all("div", {"data-url": True})
-
-    for div in divs:
-        version_span = div.find("span", class_="version")
-        if version_span and version_span.text.strip() == version:
-            dl_page = div["data-url"]
-            dl_url = dl_page.replace('/download/', '/post-download/')
-            response = scraper.get(dl_url)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.content, "html.parser")
-            post_download_divs = soup.find_all("div", class_="post-download")
-            for div in post_download_divs:
-                data_url = div["data-url"]
-                full_url = "https://dw.uptodown.com/dwn/" + data_url
-                return full_url
+    version_div = soup.find("div", {"data-url": f"/download/youtube-music-{version}"})
+    if version_div:
+        dl_page = version_div["data-url"]
+        dl_url = dl_page.replace('/download/', '/post-download/')
+        response = scraper.get(dl_url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, "html.parser")
+        post_download_div = soup.find("div", class_="post-download")
+        if post_download_div:
+            data_url = post_download_div["data-url"]
+            full_url = "https://dw.uptodown.com/dwn/" + data_url
+            return full_url
 
     return None
 
