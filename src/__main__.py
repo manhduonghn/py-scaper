18d54e21e3eeabf761897dd278c553253a561f0f
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-08 22:13:10 +0700

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -2,25 +2,4 @@ import logging
 from src import downloader
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
-    apps = ["reddit", "x", "tiktok", "youtube", "youtube-music"]
-
-    for app in apps:
-        result = downloader.download_apkmirror(app)
-        if result:
-            logging.info(f"Downloaded {app} from APKMirror successfully.")
-        else:
-            logging.error(f"Failed to download {app} from APKMirror.")
-        
-        result = downloader.download_uptodown(app)
-        if result:
-            logging.info(f"Downloaded {app} from Uptodown successfully.")
-        else:
-            logging.error(f"Failed to download {app} from Uptodown.")
-        
-        result = downloader.download_apkpure(app)
-        if result:
-            logging.info(f"Downloaded {app} from APKPure successfully.")
-        else:
-            logging.error(f"Failed to download {app} from APKPure.")
+    apk = downloader.download_apkmirror("reddit")
