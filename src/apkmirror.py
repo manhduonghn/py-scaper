15898295c8a5917d169e70b4cc05e8a439984652
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-09 05:16:12 +0700

    Update apkmirror.py

--- src/apkmirror.py
+++ src/apkmirror.py
@@ -13,7 +13,7 @@ scraper.headers.update(
     {'User-Agent': 'Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0'}
 )
 logging.basicConfig(
-  level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+  level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 def get_download_page(version: str, app_name: str) -> str:
