e921be20d42cffd89e2f1f86f2dd671919b38fd1
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-08 21:57:54 +0700

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -4,18 +4,19 @@ import json
 import logging
 from src import downloader
 
-apk = downloader.download_apkmirror("reddit")
-downloader.download_apkmirror("x")
-downloader.download_apkmirror("tiktok")
-downloader.download_apkmirror("youtube")
-downloader.download_apkmirror("youtube-music")
-downloader.download_uptodown("tiktok")
-downloader.download_uptodown("youtube")
-downloader.download_uptodown("youtube-music")
-downloader.download_apkpure("reddit")
-downloader.download_apkpure("x")
-downloader.download_apkpure("tiktok")
-downloader.download_apkpure("youtube")
-downloader.download_apkpure("youtube-music")
+if __name__ == "__main__":
+    downloader.download_apkmirror("reddit")
+    downloader.download_apkmirror("x")
+    downloader.download_apkmirror("tiktok")
+    downloader.download_apkmirror("youtube")
+    downloader.download_apkmirror("youtube-music")
+    downloader.download_uptodown("tiktok")
+    downloader.download_uptodown("youtube")
+    downloader.download_uptodown("youtube-music")
+    downloader.download_apkpure("reddit")
+    downloader.download_apkpure("x")
+    downloader.download_apkpure("tiktok")
+    downloader.download_apkpure("youtube")
+    downloader.download_apkpure("youtube-music")
 
   
