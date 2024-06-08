3b2be1cba92c8c711944f4b81c0c09607081b679
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-08 22:01:20 +0700

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -5,18 +5,11 @@ import logging
 from src import downloader
 
 if __name__ == "__main__":
-    apk = downloader.download_apkmirror("reddit")
-    downloader.download_apkmirror("x")
-    downloader.download_apkmirror("tiktok")
-    downloader.download_apkmirror("youtube")
-    downloader.download_apkmirror("youtube-music")
-    downloader.download_uptodown("tiktok")
-    downloader.download_uptodown("youtube")
-    downloader.download_uptodown("youtube-music")
-    downloader.download_apkpure("reddit")
-    downloader.download_apkpure("x")
-    downloader.download_apkpure("tiktok")
-    downloader.download_apkpure("youtube")
-    downloader.download_apkpure("youtube-music")
+    logging.basicConfig(level=logging.INFO)
 
-  
+    apps = ["reddit", "x", "tiktok", "youtube", "youtube-music"]
+
+    for app in apps:
+        downloader.download_apkmirror(app)
+        downloader.download_uptodown(app)
+        downloader.download_apkpure(app)
