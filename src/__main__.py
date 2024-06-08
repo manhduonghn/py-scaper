597f8b6bc350b0e32ad770706bd337c29887816b
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-08 21:47:42 +0700

    Create __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import json
+import logging
+from src import downloader
+
+downloader.download_apkmirror("reddit")
+downloader.download_apkmirror("x")
+downloader.download_apkmirror("tiktok")
+downloader.download_apkmirror("youtube")
+downloader.download_apkmirror("youtube-music")
+downloader.download_uptodown("tiktok")
+downloader.download_uptodown("youtube")
+downloader.download_uptodown("youtube-music")
+downloader.download_apkpure("reddit")
+downloader.download_apkpure("x")
+downloader.download_apkpure("tiktok")
+downloader.download_apkpure("youtube")
+downloader.download_apkpure("youtube-music")
+
+  
