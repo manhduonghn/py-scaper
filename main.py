48a36b9259af8ead2709b5874fb316c4f5efc4d3
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-09 00:03:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.apkmirror import download_apkmirror
-from src.apkpure import download_apkpure‎
+from src.apkpure import download_apkpure
 from src.uptodown import download_uptodown
 
 download_apkpure('youtube')
