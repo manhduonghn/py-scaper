5a08fd1020ed18523d33c82b7e268ca4ab1a65c1
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-09 00:11:18 +0700

    Update apkmirror.py

--- src/apkmirror.py
+++ src/apkmirror.py
@@ -106,5 +106,3 @@ def download_apkmirror(app_name: str) -> str:
     filename = f"{app_name}-v{version}.apk"
     download_resource(download_link, filename)
     logging.info(f"Downloaded file saved as {filename}")
-
-download_apkmirror('youtube')
