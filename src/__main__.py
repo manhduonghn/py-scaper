3175500b5982be161934ed3f679b9576c86276fa
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-08 22:18:47 +0700

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,17 @@
+import os
 import logging
 from src import downloader
 
+def run_build(app_name: str) -> str:
+    if source == "apkmirror":
+        return downloader.download_apkmirror(app_name)
+    elif source == "apkpure":
+        return downloader.download_apkpure(app_name)
+    elif source == "uptodown":
+        return downloader.download_uptodown(app_name)
+    else:
+        logging.error("Unknown source")
+        return None
+
 if __name__ == "__main__":
-    apk = downloader.download_apkmirror("reddit")
+    apk_filepath = run_build("youtube")
