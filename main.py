13b2c703e344fab3a1807c8f4569972660fa955a
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-09 00:01:02 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.apkmirror import download_apkmirror
+from src.apkpure import download_apkpure‎
+from src.uptodown import download_uptodown
+
+download_apkpure('youtube')
+download_uptodown('youtube')
+download_apkmirror('youtube')
