6d4687221cddc94d8c3bd30c0a16dc01172147ad
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-09 05:12:51 +0700

    Update apkpure.py

--- src/apkpure.py
+++ src/apkpure.py
@@ -8,7 +8,7 @@ scraper.headers.update(
     {'User-Agent': 'Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0'}
 )
 logging.basicConfig(
-  level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+  level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
 )
       
 def get_latest_version(app_name: str) -> str:
