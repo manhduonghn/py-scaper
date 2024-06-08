46c31242099a6f45e661e4284d17055755e2e7b1
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-08 23:52:35 +0700

    Update apkpure.py

--- src/apkpure.py
+++ src/apkpure.py
@@ -1,7 +1,15 @@
 import json
+import logging
 
-from src import scraper 
 from bs4 import BeautifulSoup
+
+scraper = cloudscraper.create_scraper()
+scraper.headers.update(
+    {'User-Agent': 'Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0'}
+)
+logging.basicConfig(
+  level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+)
       
 def get_latest_version(app_name: str) -> str:
     conf_file_path = f'./apps/apkpure/{app_name}.json'   
@@ -39,3 +47,32 @@ def get_download_link(version: str, app_name: str) ->str:
         return download_link['href']
     
     return None
+
+def download_resource(url: str, name: str) -> str:
+    filepath = f"./{name}"
+
+    with scraper.get(url, stream=True) as res:
+        res.raise_for_status()
+
+        total_size = int(res.headers.get('content-length', 0))
+        downloaded_size = 0
+
+        with open(filepath, "wb") as file:
+            for chunk in res.iter_content(chunk_size=8192):
+                file.write(chunk)
+                downloaded_size += len(chunk)
+
+        logging.info(
+            f"URL: {url} [{downloaded_size}/{total_size}] -> {name}"
+        )
+
+    return filepath
+
+def download_apkpure(app_name: str) -> str:
+    version = get_latest_version(app_name)
+    download_link = get_download_link(version, app_name)
+    filename = f"{app_name}-v{version}.apk"
+    download_resource(download_link, filename)
+    logging.info(f"Downloaded file saved as {filename}")
+
+download_apkpure('youtube')
