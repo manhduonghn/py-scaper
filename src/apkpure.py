925453e3448b69b7d418ec462baf92af62a2ab78
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-09 07:53:01 +0700

    Update apkpure.py

--- src/apkpure.py
+++ src/apkpure.py
@@ -9,12 +9,8 @@ scraper.headers.update(
     {'User-Agent': 'Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0'}
 )
 logging.basicConfig(
-    level=logging.INFO, format='%(asctime)s URL:%(message)s [1]', datefmt='%Y-%m-%d %H:%M:%S'
+  level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
 )
-
-def log_response(response, name):
-    content_size = len(response.content)
-    logging.info(f"{response.url} [{content_size}/{content_size}] -> \"{name}\"")
 
 def get_latest_version(app_name: str) -> str:
     conf_file_path = f'./apps/apkpure/{app_name}.json'   
@@ -25,7 +21,8 @@ def get_latest_version(app_name: str) -> str:
 
     response = scraper.get(url)
     response.raise_for_status()
-    log_response(response, "-")
+    content_size = len(response.content)
+    logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
     soup = BeautifulSoup(response.content, "html.parser")
     version_info = soup.find('div', class_='ver-top-down')
 
@@ -45,7 +42,8 @@ def get_download_link(version: str, app_name: str) -> str:
 
     response = scraper.get(url)
     response.raise_for_status()
-    log_response(response, "-")
+    content_size = len(response.content)
+    logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
     soup = BeautifulSoup(response.content, "html.parser")
     download_link = soup.find(
         'a', href=lambda href: href and f"/APK/{config['package']}" in href
@@ -71,7 +69,7 @@ def download_resource(url: str, name: str) -> str:
                 downloaded_size += len(chunk)
                 
         logging.info(
-            f"{final_url} [{downloaded_size}/{total_size}] -> \"{name}\""
+            f"URL:{final_url} [{downloaded_size}/{total_size}] -> \"{name}\" [1]"
         )
 
     return filepath
