cecbbcea19e1388dc2b6a055dfce446308a4145b
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-10 07:27:59 +0700

    Update apkmirror.py

--- src/apkmirror.py
+++ src/apkmirror.py
@@ -2,88 +2,77 @@ import os
 import re
 import json
 import logging
-import cloudscraper
-
-from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
 # Configuration
 base_url = "https://www.apkmirror.com"
-scraper = cloudscraper.create_scraper()
-scraper.headers.update(
+session = HTMLSession()
+session.headers.update(
     {'User-Agent': 'Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0'}
 )
 logging.basicConfig(
   level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-    
 def get_download_page(version: str, app_name: str) -> str:
-    conf_file_path = f'./apps/apkmirror/{app_name}.json'   
+    conf_file_path = f'./apps/apkmirror/{app_name}.json'
     with open(conf_file_path, 'r') as json_file:
         config = json.load(json_file)
 
     criteria = [config['type'], config['arch'], config['dpi']]
     url = (f"{base_url}/apk/{config['org']}/{config['name']}/"
            f"{config['name']}-{version.replace('.', '-')}-release/")
-    response = scraper.get(url)
+    response = session.get(url)
     response.raise_for_status()
     content_size = len(response.content)
     logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
-    
-    soup = BeautifulSoup(response.content, "html.parser")
 
-    rows = soup.find_all('div', class_='table-row headerFont')
+    rows = response.html.find('div.table-row.headerFont')
     for row in rows:
-        row_text = row.get_text()
+        row_text = row.text
         if all(criterion in row_text for criterion in criteria):
-            sub_url = row.find('a', class_='accent_color')
+            sub_url = row.find('a.accent_color', first=True)
             if sub_url:
-                return base_url + sub_url['href']
+                return base_url + sub_url.attrs['href']
     return None
 
 def extract_download_link(page: str) -> str:
-    response = scraper.get(page)
+    response = session.get(page)
     response.raise_for_status()
     content_size = len(response.content)
     logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
-    
-    soup = BeautifulSoup(response.content, "html.parser")
 
-    sub_url = soup.find('a', class_='downloadButton')
+    sub_url = response.html.find('a.downloadButton', first=True)
     if sub_url:
-        download_page_url = base_url + sub_url['href']
-        response = scraper.get(download_page_url)
+        download_page_url = base_url + sub_url.attrs['href']
+        response = session.get(download_page_url)
         response.raise_for_status()
         content_size = len(response.content)
         logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
-    
-        soup = BeautifulSoup(response.content, "html.parser")
 
-        sub_url = soup.select_one('a[rel="nofollow"]')
+        sub_url = response.html.find('a[rel="nofollow"]', first=True)
         if sub_url:
-            return base_url + sub_url['href']
+            return base_url + sub_url.attrs['href']
 
     return None
 
 def get_latest_version(app_name: str) -> str:
-    conf_file_path = f'./apps/apkmirror/{app_name}.json'    
+    conf_file_path = f'./apps/apkmirror/{app_name}.json'
     with open(conf_file_path, 'r') as json_file:
         config = json.load(json_file)
-        
+
     url = f"{base_url}/uploads/?appcategory={config['name']}"
 
-    response = scraper.get(url)
+    response = session.get(url)
     response.raise_for_status()
     content_size = len(response.content)
     logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
-    
-    soup = BeautifulSoup(response.content, "html.parser")
 
-    app_rows = soup.find_all("div", class_="appRow")
+    app_rows = response.html.find("div.appRow")
     version_pattern = re.compile(r'\d+(\.\d+)*(-[a-zA-Z0-9]+(\.\d+)*)*')
 
     for row in app_rows:
-        version_text = row.find("h5", class_="appRowTitle").a.text.strip()
+        version_text = row.find("h5.appRowTitle a", first=True).text.strip()
         if "alpha" not in version_text.lower() and "beta" not in version_text.lower():
             match = version_pattern.search(version_text)
             if match:
@@ -94,10 +83,10 @@ def get_latest_version(app_name: str) -> str:
 def download_resource(url: str, name: str) -> str:
     filepath = f"./{name}"
 
-    with scraper.get(url, stream=True) as res:
+    with session.get(url, stream=True) as res:
         res.raise_for_status()
 
-        final_url = res.url 
+        final_url = res.url
         total_size = int(res.headers.get('content-length', 0))
         downloaded_size = 0
 
@@ -105,16 +94,16 @@ def download_resource(url: str, name: str) -> str:
             for chunk in res.iter_content(chunk_size=8192):
                 file.write(chunk)
                 downloaded_size += len(chunk)
-                
+
         logging.info(
             f"URL:{final_url} [{downloaded_size}/{total_size}] -> \"{name}\" [1]"
         )
 
     return filepath
-    
+
 def download_apkmirror(app_name: str) -> str:
     version = get_latest_version(app_name)
-    download_page = get_download_page(version, app_name) 
+    download_page = get_download_page(version, app_name)
     download_link = extract_download_link(download_page)
     filename = f"{app_name}-v{version}.apk"
     return download_resource(download_link, filename)
