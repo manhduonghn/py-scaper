04d619ca1081bd9d7f7702c92b105a95442b6f16
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-10 22:47:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(
 )
 
 def get_download_link(version: str) -> str:
-    url = f"https://apkcombo.com/youtube/com.google.android.youtube/download/phone-{version}-apk"
+    url = f"https://apkcombo.com/vi/youtube/com.google.android.youtube/download/phone-{version}-apk"
     response = session.get(url)
     response.raise_for_status()
     content_size = len(response.content)
