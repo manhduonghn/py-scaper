98fe168ebee6be6ce012b62df87cc826b2277144
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-10 22:51:18 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-import json
-import logging
-from requests_html import HTMLSession
+from src.apkmirror import download_apkmirror
+from src.apkpure import download_apkpure
+from src.uptodown import download_uptodown
 
-# Configuration
-session = HTMLSession()
-logging.basicConfig(
-  level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
-)
-
-def get_download_link(version: str) -> str:
-    url = f"https://apkcombo.com/vi/youtube/com.google.android.youtube/download/phone-{version}-apk"
-    response = session.get(url)
-    response.raise_for_status()
-    content_size = len(response.content)
-    logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
-    download_link = response.html.find('a', containing="com.apk?", first=True)
-    if download_link:
-        return download_link.attrs['href']
-    return None
-
-version = '19.23.33'
-url = get_download_link(version)
-
-logging.info(url)
+download_apkpure('x')
+download_apkpure('youtube')
+download_apkpure('youtube-music')
+download_uptodown('youtube')
+download_uptodown('youtube-music')
+download_uptodown('tiktok')
+download_apkmirror('youtube')
+download_apkmirror('youtube-music')
+download_apkmirror('x')
