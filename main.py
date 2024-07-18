b204c20efaf7a3739582a2ff5e19d97892a1a39c
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-07-18 16:53:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.uptodown import download_uptodown
 download_apkpure('x')
 download_apkpure('youtube')
 download_apkpure('youtube-music')
-# download_apkpure('soundcloud')
+download_apkpure('soundcloud')
 download_uptodown('youtube')
 download_uptodown('youtube-music')
 download_uptodown('tiktok')
