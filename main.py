82b1af106e2285fea119c37afb2b09f454a7675c
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-07-24 12:00:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,4 @@ from src.apkmirror import download_apkmirror
 from src.apkpure import download_apkpure
 from src.uptodown import download_uptodown
 
-download_apkpure('x')
-download_apkpure('youtube')
-download_apkpure('youtube-music')
-download_apkpure('soundcloud')
-download_uptodown('youtube')
-download_uptodown('youtube-music')
-download_uptodown('tiktok')
 download_uptodown('soundcloud')
-download_apkmirror('youtube')
-download_apkmirror('youtube-music')
-download_apkmirror('x')
-download_apkmirror('soundcloud')
