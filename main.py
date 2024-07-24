c51101e8797b5b7573747933f5827a40dcaf7a49
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-07-24 13:19:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from src.apkmirror import download_apkmirror
 from src.apkpure import download_apkpure
 from src.uptodown import download_uptodown
 
-download_uptodown('soundcloud')
+download_uptodown('x')
