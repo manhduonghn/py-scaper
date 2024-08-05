203cc46263cf15aaaf44563affe046952ca16881
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-08-05 19:18:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.apkmirror import download_apkmirror
 from src.apkpure import download_apkpure
 from src.uptodown import download_uptodown
 
-download_apkpure('x')
+#download_apkpure('x')
 download_apkpure('youtube')
 download_apkpure('youtube-music')
 download_apkpure('soundcloud')
@@ -12,5 +12,5 @@ download_uptodown('tiktok')
 download_uptodown('soundcloud')
 download_apkmirror('youtube')
 download_apkmirror('youtube-music')
-download_apkmirror('x')
+#download_apkmirror('x')
 download_apkmirror('soundcloud')
