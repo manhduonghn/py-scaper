0e7f6dda530331f3399e55d116a3b37b29603ca1
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-10-12 19:26:19 +0700

    Update uptodown.py

--- src/uptodown.py
+++ src/uptodown.py
@@ -10,7 +10,7 @@ import os
 
 # Configuration
 logging.basicConfig(
-    level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+    level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 user_agents = [
