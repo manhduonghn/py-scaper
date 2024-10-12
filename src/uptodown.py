ed726b043ec9b19af418d79c55d169ac88876f4e
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-10-12 19:35:40 +0700

    Update uptodown.py

--- src/uptodown.py
+++ src/uptodown.py
@@ -10,7 +10,7 @@ import os
 
 # Configuration
 logging.basicConfig(
-    level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+    level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 # Create Chrome driver with headless options
