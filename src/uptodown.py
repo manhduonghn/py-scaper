b82843565fbf4d5cc62f0e3d1f2a6c2689cf411d
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-10-12 19:37:33 +0700

    Update uptodown.py

--- src/uptodown.py
+++ src/uptodown.py
@@ -133,7 +133,8 @@ def download_resource(url: str, name: str) -> str:
 
 # Main function to download app from Uptodown
 def download_uptodown(app_name: str) -> str:
-    version = get_latest_version(app_name)
+    version = "19.04.36"
+    # version = get_latest_version(app_name)
     download_link = get_download_link(version, app_name)
     filename = f"{app_name}-v{version}"
     return download_resource(download_link, filename)
