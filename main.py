fc6674b36af47fc18ca7cfe76e740c62671cb612
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-10-12 19:09:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from src.apkmirror import download_apkmirror
-from src.apkpure import download_apkpure
+#from src.apkmirror import download_apkmirror
+#from src.apkpure import download_apkpure
 from src.uptodown import download_uptodown
 
 #download_apkpure('x')
