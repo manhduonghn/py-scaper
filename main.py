84209d5797787f71409b4c9c43b2f91d4ceee0df
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-10-15 20:46:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-#from src.apkmirror import download_apkmirror
-#from src.apkpure import download_apkpure
-from src.uptodown import download_uptodown
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 
-#download_apkpure('x')
-#download_apkpure('youtube')
-#download_apkpure('youtube-music')
-#download_apkpure('soundcloud')
-download_uptodown('youtube')
-#download_uptodown('youtube-music')
-#download_uptodown('tiktok')
-#download_uptodown('soundcloud')
-#download_apkmirror('youtube')
-#download_apkmirror('youtube-music')
-#download_apkmirror('x')
-#download_apkmirror('soundcloud')
+# Set up Chrome options and the WebDriver
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run in headless mode (no browser UI)
+service = Service('/usr/bin/chromedriver')  # Specify the path to your chromedriver
+chrome_options.add_argument(
+        f"user-agent=Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0"
+)
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+# Navigate to the webpage
+url = "https://apkcombo.com/vi/youtube/com.google.android.youtube/download/phone-19.16.39-apk"
+driver.get(url)
+
+# Locate the download button by its class and click on it
+try:
+    # Use the class name for the download button
+    download_button = driver.find_element(By.CSS_SELECTOR, '.download-button')
+    # Scroll into view if needed
+    actions = ActionChains(driver)
+    actions.move_to_element(download_button).perform()
+    # Click the button
+    download_button.click()
+    print("Download link clicked successfully!")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+# Close the browser
+driver.quit()
