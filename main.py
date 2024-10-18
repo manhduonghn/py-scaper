3db34b59da04c9c1a405d2833e0202193bcd8c89
manhduonghn <manhduonghn@live.com>
2024-10-18 15:02:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import logging 
+
 from src.uptodown import (
     download_uptodown,
     download_assets_from_repo
 )
 
 download_uptodown('youtube')
+
+url = f'https://github.com/REAndroid/APKEditor/releases/latest'
+
+editor = download_assets_from_repo(url)
+logging.info(f"{editor}")
