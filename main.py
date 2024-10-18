88498a11e4fb7593cccf2011434168a70a4a7691
manhduonghn <manhduonghn@live.com>
2024-10-18 14:57:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-#from src.apkmirror import download_apkmirror
-#from src.apkpure import download_apkpure
-from src.uptodown import download_uptodown
+from src.uptodown import (
+    download_uptodown,
+    download_assets_from_repo
+)
 
-#download_apkpure('x')
-#download_apkpure('youtube')
-#download_apkpure('youtube-music')
-#download_apkpure('soundcloud')
 download_uptodown('youtube')
-#download_uptodown('youtube-music')
-#download_uptodown('tiktok')
-#download_uptodown('soundcloud')
-#download_apkmirror('youtube')
-#download_apkmirror('youtube-music')
-#download_apkmirror('x')
-#download_apkmirror('soundcloud')
